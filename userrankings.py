9489fb0b7458fdcc63d31baf241a481206fa789e
Jay Kominek <kominek@gmail.com>
2014-06-15 13:18:12 -0600

    support for more intelligentlly getting a new day's ranking info

--- userrankings.py
+++ userrankings.py
@@ -14,7 +14,7 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 import sqlite3
 
-conn = sqlite3.connect("rankings.db")
+conn = sqlite3.connect("rankings.db", detect_types=sqlite3.PARSE_DECLTYPES)
 c = conn.cursor()
 
 NOW = datetime.now()
@@ -112,10 +112,18 @@ def by_rank(pos, previous=None):
     return by_id(record[0])
 
 def fillout_top(n):
+    have = { }
+    seen_ids = set([])
     needed = set(range(1, n+1))
-    c.execute("select rank from rankings where rank<=? and day=?", (n, CURRENT_DAY))
-    for (rank,) in c:
+    c.execute("select rank, id from rankings where rank<=? and day=?", (n, CURRENT_DAY))
+    for (rank, id) in c:
         needed.remove(rank)
+        have[rank] = id
+
+    good_candidates = set([])
+    c.execute("select rank, id from rankings where 10<rank and rank<=? and day=?", (n, CURRENT_DAY - timedelta(1)))
+    for rank, id in c:
+        pass
 
     for rank in needed:
         by_rank(rank)
@@ -135,6 +143,44 @@ def dump_plot_today():
         f.write("%i %f\n" % (rank, score))
     f.close()
 
+def parsedate(s):
+    if type(s) == date:
+        return s
+    return date(*map(int, s.split("-")))
+
+# plot "rankings" using ($1):(-log10($2)) with lines
+# plot "scores" with lines
+def dump_historical_plots():
+    c.execute("select min(day) from rankings")
+    start_day = parsedate(c.fetchone()[0])
+    rankings = { }
+    scores = { }
+    c.execute("select id, day, rank, score from rankings where score!=5000.0 and score!=1000.0 order by day")
+    for id, day, rank, score in c:
+        if rank != None:
+            l = rankings.setdefault(id, [ ])
+            l.append( (parsedate(day), rank) )
+
+        if score != None:
+            l = scores.setdefault(id, [ ])
+            l.append( (parsedate(day), score) )
+
+    f = open("rankings", "w")
+    for id in rankings.keys():
+        for d, v in rankings[id]:
+            f.write("%i %f\n" % ((d-start_day).days, v))
+        f.write("\n")
+    f.close()
+
+    f = open("scores", "w")
+    for id in scores.keys():
+        for d, v in scores[id]:
+            f.write("%i %f\n" % ((d-start_day).days, v))
+        f.write("\n")
+    f.close()
+
+
+
 def find_active_users():
     recent_users = { }
     for f in os.listdir("q"):
@@ -148,8 +194,67 @@ def find_active_users():
     return recent_users.keys()
 
 def fetch_active_users():
-    for user_id in find_active_users():
+    for user_id in sorted(find_active_users(), key=min_score):
         by_id(user_id)
+
+def random_expansion():
+    rank_to_id = { }
+    c.execute("select rank, id from rankings where score>5000.0 and day=?", (CURRENT_DAY,))
+    for rank, id in c:
+        rank_to_id[rank] = id
+
+    candidates = set([])
+    for rank in rank_to_id.keys():
+        if rank<=10:
+            continue
+        if rank_to_id.has_key(rank+1) and rank_to_id.has_key(rank-1):
+            pass
+        else:
+            candidates.add(rank_to_id[rank])
+    print "found %i random expansion candidates" % (len(candidates),)
+    for id in random.sample(candidates, min(10, len(candidates))):
+        fetch_and_update(id)
+
+def fetch_smattering():
+    already_fetched = set([])
+    c.execute("select id from rankings where day=?", (CURRENT_DAY,))
+    for (id,) in c:
+        already_fetched.add(id)
+
+    high_end = set([])
+    c.execute("select id from rankings where day<? and rank<10 and score>5000.0", (CURRENT_DAY,))
+    for (id,) in c:
+        if id not in already_fetched:
+            high_end.add(id)
+
+    low_end = set([])
+    c.execute("select id from rankings where day<? and 1000.0<score and score<5000.0", (CURRENT_DAY,))
+    for (id,) in c:
+        if not (id in already_fetched or id in high_end):
+            low_end.add(id)
+
+    fiveks = set([])
+    c.execute("select id from rankings where day<? and score=5000.0", (CURRENT_DAY,))
+    for (id,) in c:
+        if not (id in already_fetched or id in high_end or id in low_end):
+            fiveks.add(id)
+
+    print "found %i for high end" % (len(high_end),)
+    print "found %i for 5ks" % (len(fiveks),)
+    print "found %i for low end" % (len(low_end),)
+
+    for id in random.sample(high_end, min(len(high_end), 10)):
+        by_id(id)
+    for id in random.sample(fiveks, min(len(fiveks), 5)):
+        by_id(id)
+    for id in random.sample(low_end, min(len(low_end), 10)):
+        by_id(id)
+
+def update_for_new_day():
+    fetch_active_users()
+    fillout_top(20)
+    fetch_smattering()
+    random_expansion()
 
 def min_score(user_id):
     c.execute("select min(score) from rankings where id=? and ?<=day",
@@ -159,4 +264,4 @@ def min_score(user_id):
         by_id(user_id)
     c.execute("select min(score) from rankings where id=? and ?<=day",
               (user_id, CURRENT_DAY - timedelta(7)))
-    return c.fetchone()[0]
+    return max(1.0, c.fetchone()[0])
