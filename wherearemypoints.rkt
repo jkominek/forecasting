0a9f0a522d9aec52e0774a1117d972190cc9298c
Jay Kominek <kominek@gmail.com>
2014-08-28 22:25:36 -0600

    sort by resolution time

--- wherearemypoints.rkt
+++ wherearemypoints.rkt
@@ -3,9 +3,12 @@
 (require (file "/home/jkominek/forecasting/questions.rkt")
 	 (file "/home/jkominek/forecasting/opinions.rkt")
 	 (file "/home/jkominek/forecasting/utils.rkt")
+         (except-in (planet bzlib/date/plt) date->string)
+         (only-in racket/date date->string date-display-format)
 	 )
 
 (define my-user-id (make-parameter 296))
+(define sort-by (make-parameter 'tied-up))
 
 (void
  (command-line
@@ -14,6 +17,9 @@
   [("--uid") user-id
    "User ID to investigate"
    (my-user-id (string->number user-id))]
+  [("--resolution")
+   "Sort by resolution time"
+   (sort-by 'resolution)]
   ))
 
 (question-database
@@ -40,15 +46,34 @@
     (hash-set! tied-up q-id (apply min standing))
     ))
 
-(for ([q-id (sort (hash-keys tied-up)
-		  > ; reverse
-		  #:key (lambda (k) (hash-ref tied-up k)))])
+(define display-order
+  (cond
+   [(equal? (sort-by) 'resolution)
+    (sort (hash-keys tied-up)
+          date>?
+          #:key
+          (lambda (k) (question-settlement-at (fetch-question k))))]
+   [else
+    (sort (hash-keys tied-up)
+          >
+          #:key (lambda (k) (hash-ref tied-up k)))]))
+
+(date-display-format 'iso-8601)
+
+(for/fold
+ ([current-date (date->string (question-settlement-at (fetch-question (car display-order))))])
+ ([q-id display-order]) 
   (define q (fetch-question q-id))
-  (define limit (maximum-points-tied-up (question-settlement-at q)))
+  (define settlement (question-settlement-at q))
+  (define settlements (date->string settlement))
+  (define limit (maximum-points-tied-up settlement))
 
   ;(when (have-opinion? q-id)
   ;  (flsum (map * standing (opinion-beliefs (get-opinion q-id)))))
 
+  (when (equal? (sort-by) 'resolution)
+        (unless (equal? current-date settlements)
+                (printf "--- ~a~n" settlements)))
   (display
    (string-join
     (list (cat (hash-ref tied-up q-id) 7 2.)
@@ -58,4 +83,6 @@
 	  (string-trim #:repeat? #t (question-name q))
 	  "\n")
     ""))
+
+  settlements
   )
